e4cf078ef588a75c8ab9ea2208763e9792d16e85
Tenmei-K <jhxiang2022@outlook.com>
2026-03-06 11:18:09 +0800

    Initial commit

---
+++ 260305p5/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noFill();
+  strokeWeight(2);
+  stroke(180)
+  circle(160, 200, 200);
+  circle(240, 200, 200);
+}
